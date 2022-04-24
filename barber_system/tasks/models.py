9a4b1d4a44bfa4f01effaf2154c1d9776dc775e2
pngabriele <gabriele.uau@hotmail.com>
2022-04-24 18:52:17 -0300

    criado model para o tipo de corte

--- barber_system/tasks/models.py
+++ barber_system/tasks/models.py
@@ -1,8 +1,10 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.contrib.auth import get_user_model
 
 
 # Create your models here.
+
 
 class Task(models.Model):
     STATUS_CHOICES = (
@@ -23,3 +25,25 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Corte(models.Model):
+    DURACAO_CHOICES = (
+        ('1', '00:30'),
+        ('2', '01:00'),
+        ('3', '01:30'),
+        ('4', '02:00')
+    )
+    NOME_CHOICES = (
+        ('corte', 'Corte'),
+        ('barba', 'Barba'),
+        ('sobrancelha', 'Sobrancelha'),
+        ('corte_sobrancelha', 'Corte e sobrancelha'),
+        ('corte_barba', 'Corte e barba'),
+        ('barba_sobrancelha', 'Barba e Sobrancelha'),
+        ('corte_barba_sobrancelha', 'Corte,barba e sobrancelha'),
+    )
+
+    nome = models.CharField('Nome', max_length=255, null=False, blank=False, choices=NOME_CHOICES)
+    duracao = models.CharField('Duração', max_length=50, null=False, blank=False, choices=DURACAO_CHOICES)
+    preco = models.FloatField('Preço', null=False, blank=False, validators=[MinValueValidator(0.0)])
