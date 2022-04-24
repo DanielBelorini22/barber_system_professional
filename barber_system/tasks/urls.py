feb6418d6867234d1571ef3b8e8deae94c14151c
pngabriele <gabriele.uau@hotmail.com>
2022-04-24 18:51:54 -0300

    alterado nome das funções conforme convenção e criado rota para os cortes

--- barber_system/tasks/urls.py
+++ barber_system/tasks/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.taskList, name='task-list'),
-    path('task/<int:pk>/', views.taskView, name='task-view'),
-    path('newtask/', views.newTask, name='new-task'),
-    path('edit/<int:pk>/', views.editTask, name='edit-task'),
-    path('changestatus/<int:pk>/', views.changeStatus, name='change-status'),
-    path('delete/<int:pk>/', views.deleteTask, name='delete-task'),
+    path('', views.task_list, name='task-list'),
+    path('cortes/', views.tipo_cortes, name='tipo-cortes'),
+    path('cortes/novo/', views.novo_tipo_cortes, name='tipo-cortes-novo'),
+    path('dashboard/', views.dash_board, name='dashboard'),
+    path('task/<int:pk>/', views.task_view, name='task-view'),
+    path('newtask/', views.new_task, name='new-task'),
+    path('edit/<int:pk>/', views.edit_task, name='edit-task'),
+    path('changestatus/<int:pk>/', views.change_status, name='change-status'),
+    path('delete/<int:pk>/', views.delete_task, name='delete-task'),
 ]
