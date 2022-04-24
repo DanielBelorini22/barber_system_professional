7d63b798611f56ccfa7fb3846a97a20132e24ead
pngabriele <gabriele.uau@hotmail.com>
2022-04-24 18:52:40 -0300

    criado formulário para o model do tipo de corte

--- barber_system/tasks/forms.py
+++ barber_system/tasks/forms.py
@@ -1,10 +1,13 @@
-
 from django import forms
 
-from .models import Task
+from .models import Corte
 
 
-class TaskForm(forms.ModelForm):
+class CorteForm(forms.ModelForm):
     class Meta:
-        model = Task
-        fields = ('title', 'description')
+        model = Corte
+        fields = (
+            'nome',
+            'duracao',
+            'preco',
+        )
