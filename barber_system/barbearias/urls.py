2dd1ab96b444c24c70005472b800c30605510733
lucasfiametti <89356004+lucasfiametti@users.noreply.github.com>
2022-04-25 19:35:52 -0300

    contatos

--- barber_system/barbearias/urls.py
+++ barber_system/barbearias/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import *
-
 urlpatterns = [
     path('', home, name="home"),
     path('barbearia/listar/', listar_barbearia, name="listar_barbearia"),
+    path('barbearia/contato/', contato_barber_system, name="contato_barber_system"),
     path('barbearia/perfil/<int:pk>/', perfil_barbearia, name="perfil_barbearia"),
 ]
--- barber_system/barbearias/views.py
+++ barber_system/barbearias/views.py
@@ -29,6 +29,28 @@ def listar_barbearia(request, template_name='barbearia_list.html'):
     return render(request, template_name, barbearias)
 
 
+def contato_barber_system (request, template_name='contato_barber_system.html'):
+    query = request.GET.get('busca', '')
+    page = request.GET.get('page', '')
+    ordenar = request.GET.get('ordenar', '')
+    if query:
+        barbearia = Barbearia.objects.filter(nome_fantasia__icontains=query)
+    else:
+        try:
+            if ordenar:
+                barbearia = Barbearia.objects.all().order_by(ordenar)
+            else:
+                barbearia = Barbearia.objects.all()
+            barbearia = Paginator(barbearia, 2)
+            barbearia = barbearia.page(page)
+        except PageNotAnInteger:
+            barbearia = barbearia.page(1)
+        except EmptyPage:
+            barbearia = paginator.page(paginator.num_pages)
+    barbearias = {'lista': barbearia}
+    return render(request, template_name, barbearias)
+
+
 def perfil_barbearia(request, pk, template_name="perfil_barbearia.html"):
     barbearia = get_object_or_404(Barbearia, pk=pk)
     cortes = CortesCabelo.objects.filter(barbearia=pk)
