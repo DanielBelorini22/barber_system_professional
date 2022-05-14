5e6b7554a3d901f90cdb56b258b9a179d504ed87
pngabriele <gabriele.uau@hotmail.com>
2022-05-13 21:52:30 -0300

    ajuste na estrutura do sistema
    ajuste na estrutura do sistema

--- barber_system/tasks/forms.py
+++ barber_system/tasks/forms.py
@@ -1,10 +1,39 @@
+from crispy_forms.bootstrap import PrependedText, FormActions
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit, Button
 
 from django import forms
 
-from .models import Task
+from .models import Corte
 
 
-class TaskForm(forms.ModelForm):
+class CorteForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        helper = FormHelper()
+        helper.form_class = 'form-horizontal'
+        helper.label_class = 'col-lg-2'
+        helper.field_class = 'col-lg-8'
+
+        helper.layout = Layout(
+            Fieldset(
+                'Tipo de corte',
+                'nome',
+                'duracao',
+                PrependedText('preco', 'R$'),
+            ),
+            FormActions(
+                Submit('submit', 'Salvar', css_class='save btn btn-default'),
+                Button('cancel', 'Cancelar'),
+            )
+        )
+
+        self.helper = helper
+
     class Meta:
-        model = Task
-        fields = ('title', 'description')
+        model = Corte
+        fields = (
+            'nome',
+            'duracao',
+            'preco',
+        )
