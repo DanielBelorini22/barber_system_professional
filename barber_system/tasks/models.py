00bfccd9082893684d02ee8a94e81af9036c19b4
pngabriele <gabriele.uau@hotmail.com>
2022-05-27 18:29:32 -0300

    feat: models do corte e tipo de corte

--- barber_system/tasks/models.py
+++ barber_system/tasks/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.contrib.auth import get_user_model
@@ -27,13 +28,15 @@ class Task(models.Model):
         return self.title
 
 
-class Corte(models.Model):
-    DURACAO_CHOICES = (
-        ('1', '00:30'),
-        ('2', '01:00'),
-        ('3', '01:30'),
-        ('4', '02:00')
-    )
+DURACAO_CHOICES = (
+    (1, '00:30'),
+    (2, '01:00'),
+    (3, '01:30'),
+    (4, '02:00')
+)
+
+
+class TipoCorte(models.Model):
     NOME_CHOICES = (
         ('corte', 'Corte'),
         ('barba', 'Barba'),
@@ -45,5 +48,36 @@ class Corte(models.Model):
     )
 
     nome = models.CharField('Nome', max_length=255, null=False, blank=False, choices=NOME_CHOICES)
-    duracao = models.CharField('Duração', max_length=50, null=False, blank=False, choices=DURACAO_CHOICES)
+    duracao = models.SmallIntegerField('Duração', null=False, blank=False, choices=DURACAO_CHOICES)
     preco = models.FloatField('Preço', null=False, blank=False, validators=[MinValueValidator(0.0)])
+    desconto = models.FloatField('Desconto', null=True, validators=[MinValueValidator(0.0)])
+    ativo = models.BooleanField('Ativo', default=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.get_nome_display()} - {self.get_duracao_display()}'
+
+    def get_ativo_display(self):
+        return 'Sim' if self.ativo else 'Não'
+
+
+class Corte(models.Model):
+    STATUS_CHOICES = (
+        ('agendado', 'Agendado'),
+        ('efetuado', 'Efetuado'),
+        ('cancelado', 'Cancelado'),
+    )
+
+    cliente = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    observacao = models.TextField('Observação', max_length=200, null=False, default=str)
+    status = models.CharField('Status', max_length=25, null=False, blank=False, choices=STATUS_CHOICES,
+                              default='agendado')
+    horario = models.DateTimeField('Horário')
+    duracao = models.SmallIntegerField('Duração', null=False, blank=False, choices=DURACAO_CHOICES)
+
+    def __str__(self):
+        return f'{self.cliente} - {self.get_status_display()} - {self.get_duracao_display()}'
