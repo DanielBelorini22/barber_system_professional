97a92dfefbc008bdc24f4ceedde0268bdeff899f
pngabriele <gabriele.uau@hotmail.com>
2022-05-27 18:30:25 -0300

    feat: html da listagem de corte e tipo de corte

--- barber_system/tasks/admin.py
+++ barber_system/tasks/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Task
+from .models import Task, TipoCorte, Corte
 
 # Register your models here.
 
 admin.site.register(Task)
+admin.site.register(TipoCorte)
+admin.site.register(Corte)
