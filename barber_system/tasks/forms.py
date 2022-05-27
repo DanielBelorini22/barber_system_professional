4edfc14334f1ea5607da3954001d9aacd0252252
pngabriele <gabriele.uau@hotmail.com>
2022-05-27 18:29:45 -0300

    feat: forms.py do corte e tipo de corte

--- barber_system/tasks/forms.py
+++ barber_system/tasks/forms.py
@@ -1,13 +1,14 @@
 from crispy_forms.bootstrap import PrependedText, FormActions
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, Submit, Button
+from crispy_forms.layout import Layout, Fieldset, Submit, Button, Field
 
 from django import forms
+from django.contrib.auth.models import User
 
-from .models import Corte
+from .models import TipoCorte, Corte
 
 
-class CorteForm(forms.ModelForm):
+class TipoCorteForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         helper = FormHelper()
@@ -21,19 +22,64 @@ class CorteForm(forms.ModelForm):
                 'nome',
                 'duracao',
                 PrependedText('preco', 'R$'),
+                PrependedText('desconto', '%'),
+                'ativo',
             ),
             FormActions(
-                Submit('submit', 'Salvar', css_class='save btn btn-default'),
-                Button('cancel', 'Cancelar'),
+                Submit('submit', 'Salvar', css_class='save btn btn-success'),
+                Button('cancel', 'Cancelar', css_class='cancel btn btn-danger',
+                       onclick="javascript:history.back()"),
             )
         )
 
         self.helper = helper
 
     class Meta:
-        model = Corte
+        model = TipoCorte
         fields = (
             'nome',
             'duracao',
             'preco',
+            'desconto',
+            'ativo',
+        )
+
+
+class CorteForm(forms.ModelForm):
+    tipo_corte = forms.ModelChoiceField(TipoCorte.objects.all())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        helper = FormHelper()
+        helper.form_class = 'form-horizontal'
+        helper.label_class = 'col-lg-2'
+        helper.field_class = 'col-lg-8'
+
+        helper.layout = Layout(
+            Fieldset(
+                'Corte',
+                'cliente',
+                Field('horario', autocomplete="off"),
+                'tipo_corte',
+                'observacao',
+                'status',
+            ),
+            FormActions(
+                Submit('submit', 'Salvar', css_class='save btn btn-success'),
+                Button('cancel', 'Cancelar', css_class='cancel btn btn-danger',
+                       onclick="javascript:history.back()"),
+            )
+        )
+
+        self.helper = helper
+        self.fields['cliente'].queryset = User.objects.filter(username__endswith='o')
+
+    class Meta:
+        model = Corte
+        fields = (
+            'cliente',
+            'horario',
+            'tipo_corte',
+            'observacao',
+            'status',
         )
