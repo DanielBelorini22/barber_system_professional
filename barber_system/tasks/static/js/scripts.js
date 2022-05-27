a8682237cb4b39d6946e04615f5555fd771f2953
pngabriele <gabriele.uau@hotmail.com>
2022-05-27 18:28:57 -0300

    feat: fecha os alertas depois de 3 segundos e inicializa o datetime picker

--- barber_system/tasks/static/js/scripts.js
+++ barber_system/tasks/static/js/scripts.js
@@ -1,4 +1,18 @@
 const alertList = document.querySelectorAll('.alert');
+
 alertList.forEach(function (alert) {
-  new bootstrap.Alert(alert)
+    new bootstrap.Alert(alert)
 })
+
+$(document).ready(() => {
+    $('.datetimeinput').datetimepicker({
+        format: 'd/m/Y H:i',
+    })
+    ;
+    $.datetimepicker.setLocale('pt-BR');
+
+
+    setTimeout(() => {
+        $(".alert").alert('close');
+    }, 3000);
+});
