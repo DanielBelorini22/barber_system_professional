70a7fb0c92700e75ce652e250f8797a56c297859
pngabriele <gabriele.uau@hotmail.com>
2022-05-27 18:28:30 -0300

    feat: urls do tipo de corte e cortes

--- barber_system/tasks/urls.py
+++ barber_system/tasks/urls.py
@@ -4,9 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task-list'),
-    path('cortes/', views.tipo_cortes, name='tipo-cortes'),
-    path('cortes/novo/', views.novo_tipo_cortes, name='tipo-cortes-novo'),
-    path('cortes/editar/<int:pk>/', views.editar_tipo_cortes, name='tipo-cortes-editar'),
+    path('cortes/', views.cortes, name='cortes'),
+    path('cortes/novo/', views.novo_corte, name='cortes-novo'),
+    path('cortes/editar/<int:pk>/', views.editar_corte, name='cortes-editar'),
+    path('cortes/remover/<int:pk>/', views.remover_corte, name='cortes-remover'),
+    path('tipo-cortes/', views.tipo_cortes, name='tipo-cortes'),
+    path('tipo-cortes/novo/', views.novo_tipo_corte, name='tipo-cortes-novo'),
+    path('tipo-cortes/editar/<int:pk>/', views.editar_tipo_corte, name='tipo-cortes-editar'),
+    path('tipo-cortes/remover/<int:pk>/', views.remover_tipo_corte, name='tipo-cortes-remover'),
     path('dashboard/', views.dash_board, name='dashboard'),
     path('task/<int:pk>/', views.task_view, name='task-view'),
     path('newtask/', views.new_task, name='new-task'),
